58f6cfe81583ceaafd4e7fbd276940b5b8dc5f0d
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-04-04 23:20:10 +0300

    Runtime: 0 ms, faster than 100.00% Memory Usage: 38.7 MB, less than 83.72%

---
+++ tree/ValidateBinarySearchTree.java
@@ -0,0 +1,110 @@
+package leetcode.tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ValidateBinarySearchTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+
+        return isValidBST(root, Long.MAX_VALUE, Long.MIN_VALUE);
+
+    }
+
+    public boolean isValidBST(TreeNode root, long max, long min) {
+
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val >= max || root.val <= min) {
+            return false;
+        }
+
+        boolean solution = true;
+
+        solution &= isValidBST(root.left, root.val, min);
+        solution &= isValidBST(root.right, max, root.val);
+
+        return solution;
+
+    }
+
+    static ValidateBinarySearchTree vbst = new ValidateBinarySearchTree();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            boolean ret = vbst.isValidBST(root);
+
+            String out = booleanToString(ret);
+
+            System.out.print(out);
+        }
+    }
+
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = vbst.new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = vbst.new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = vbst.new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+
+}
