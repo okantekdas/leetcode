f72ecc97c8b7d68d0ec969320ef17645cda29677
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-04-13 14:22:11 +0300

    Runtime: 20 ms, faster than 79.10% Memory Usage: 49.1 MB, less than 100.00%

---
+++ other/ContiguousArray.java
@@ -0,0 +1,41 @@
+package leetcode.other;
+
+import java.util.HashMap;
+
+public class ContiguousArray {
+
+    public int findMaxLength(int[] nums) {
+
+        HashMap<Integer, Integer> diffToIndex = new HashMap<>();
+
+        int zeroes = 0;
+        int ones = 0;
+        int max = 0;
+        diffToIndex.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (num == 0) {
+                zeroes++;
+            } else {
+                ones++;
+            }
+
+            int diff = ones - zeroes;
+            if (diffToIndex.containsKey(diff)) {
+                max = Math.max(max, i - diffToIndex.get(diff));
+            } else {
+                diffToIndex.put(diff, i);
+            }
+        }
+
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 0, 0, 1, 1, 1, 0 };
+
+        System.out.println(new ContiguousArray().findMaxLength(nums));
+    }
+
+}
