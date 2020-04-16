4a448557d546702e5325ec5552aa8d4248b50185
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-04-17 00:03:32 +0300

    Runtime: 1 ms, faster than 100.00%

--- other/SlidingWindowMaximum.java
+++ other/SlidingWindowMaximum.java
@@ -12,30 +12,32 @@ public class SlidingWindowMaximum {
         int left = 0;
         int right = 0;
         int localMax = Integer.MIN_VALUE;
+        int prevMaxIndex = -1;
         while (right < nums.length) {
-            if (right - left == k) {
+            if (right - left + 1 == k) {
 
-                for (int i = left; i < right; i++) {
-                    if (localMax < nums[i]) {
-                        localMax = nums[i];
+                if (prevMaxIndex < 0 || prevMaxIndex < left) {
+                    localMax = Integer.MIN_VALUE;
+                    for (int i = left; i <= right; i++) {
+                        if (localMax < nums[i]) {
+                            localMax = nums[i];
+                            prevMaxIndex = i;
+                        }
+                    }
+                } else {
+                    if (nums[right] > nums[prevMaxIndex]) {
+                        localMax = nums[right];
+                        prevMaxIndex = right;
                     }
                 }
 
                 solution[left] = localMax;
-                localMax = Integer.MIN_VALUE;
+
                 left++;
             }
 
             right++;
         }
-
-        for (int i = left; i < right; i++) {
-            if (localMax < nums[i]) {
-                localMax = nums[i];
-            }
-        }
-
-        solution[left] = localMax;
 
         return solution;
 
@@ -43,10 +45,10 @@ public class SlidingWindowMaximum {
 
     public static void main(String[] args) {
 
-        int[] nums = { 1, -1 };
-        int k = 1;
-        // int[] nums = { 1, 3, -1, -3, 5, 3, 6, 7 };
-        // int k = 3;
+        // int[] nums = { 1, -1 };
+        // int k = 1;
+        int[] nums = { 1, 3, -1, -3, 5, 3, 6, 7 };
+        int k = 3;
         System.out.println(Arrays.toString(new SlidingWindowMaximum().maxSlidingWindow(nums, k)));
 
     }
