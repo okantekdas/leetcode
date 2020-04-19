3ffe4538ca4b989fd102de4b7c16fe5dce44d03d
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-04-19 23:06:45 +0300

    Runtime: 0 ms, faster than 100.00% Memory Usage: 39.6 MB, less than 81.72%

---
+++ tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+package leetcode.tree;
+
+public class MaximumDepthOfBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));
+    }
+
+}
