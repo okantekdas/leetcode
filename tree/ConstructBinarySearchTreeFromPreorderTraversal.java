4563680bc230f3e3173729c7182597e287da88fd
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-04-20 16:51:03 +0300

    Runtime: 0 ms, faster than 100.00% Memory Usage: 37.4 MB, less than 6.00%

--- tree/ConstructBinarySearchTreeFromPreorderTraversal.java
+++ tree/ConstructBinarySearchTreeFromPreorderTraversal.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/* https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/submissions/ */
 public class ConstructBinarySearchTreeFromPreorderTraversal {
 
     public class TreeNode {
