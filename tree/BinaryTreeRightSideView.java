64ecbd2c7ca9e5a819696d1e731e401807602dcf
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-04-21 13:50:58 +0300

    Runtime: 0 ms, faster than 100.00% Memory Usage: 38.4 MB, less than 5.88%

---
+++ tree/BinaryTreeRightSideView.java
@@ -0,0 +1,122 @@
+package leetcode.tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/* https://leetcode.com/problems/binary-tree-right-side-view/ */
+public class BinaryTreeRightSideView {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    List<Integer> solution = new ArrayList<>();
+
+    public List<Integer> rightSideView(TreeNode root) {
+        traverseTree(root, 0);
+        return solution;
+    }
+
+    public void traverseTree(TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+
+        if (level >= solution.size()) {
+            solution.add(root.val);
+        }
+
+        traverseTree(root.right, level + 1);
+        traverseTree(root.left, level + 1);
+
+    }
+
+    static BinaryTreeRightSideView binaryTreeRightSideView = new BinaryTreeRightSideView();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            List<Integer> ret = new BinaryTreeRightSideView().rightSideView(root);
+
+            String out = integerArrayListToString(ret);
+
+            System.out.print(out);
+        }
+    }
+
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = binaryTreeRightSideView.new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = binaryTreeRightSideView.new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = binaryTreeRightSideView.new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for (int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+
+}
