dda23cb1a728aff475ad46006e75366317945698
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-04-22 22:43:41 +0300

    Runtime: 11 ms, faster than 95.48% Memory Usage: 40 MB, less than 65.22%

---
+++ other/SubArraySumEqualsK.java
@@ -0,0 +1,45 @@
+package leetcode.other;
+
+import java.util.HashMap;
+
+public class SubArraySumEqualsK {
+
+    HashMap<Long, Integer> sumToCount = new HashMap<>();
+
+    public int subarraySum(int[] nums, int k) {
+
+        int total = 0;
+
+        long sum = 0;
+        for (int num : nums) {
+            sum += num;
+
+            if (sum == k) {
+                total++;
+            }
+
+            if (sumToCount.containsKey(sum - k)) {
+                total += sumToCount.get(sum - k);
+            }
+
+            Integer count = sumToCount.get(sum);
+            if (count == null) {
+                sumToCount.put(sum, 1);
+            } else {
+                sumToCount.put(sum, count + 1);
+            }
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = { 1, 1, 1, 2, 0, 1, 1, 3 };
+        int k = 2;
+
+        System.out.println(new SubArraySumEqualsK().subarraySum(nums, k));
+
+    }
+
+}
