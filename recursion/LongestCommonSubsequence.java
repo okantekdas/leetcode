ffdaafc9bc205711cd8b48fdbe8f02a3ce86a29b
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-04-26 19:08:07 +0300

    Runtime: 9 ms, faster than 85.95% Memory Usage: 43.6 MB, less than 100.00%

--- recursion/LongestCommonSubsequence.java
+++ recursion/LongestCommonSubsequence.java
@@ -1,82 +1,99 @@
 package leetcode.recursion;
 
-import java.util.HashMap;
-
+/* https://leetcode.com/problems/longest-common-subsequence */
 public class LongestCommonSubsequence {
-
-    public class Memo {
-        int index1;
-        int index2;
-
-        public Memo(int index1, int index2) {
-            this.index1 = index1;
-            this.index2 = index2;
-        }
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + getEnclosingInstance().hashCode();
-            result = prime * result + index1;
-            result = prime * result + index2;
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            Memo other = (Memo) obj;
-            if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
-                return false;
-            if (index1 != other.index1)
-                return false;
-            if (index2 != other.index2)
-                return false;
-            return true;
-        }
-
-        private Solution getEnclosingInstance() {
-            return Solution.this;
-        }
-
-    }
-
-    HashMap<Memo, Integer> textToLcs = new HashMap<>();
 
     public int longestCommonSubsequence(String text1, String text2) {
 
-        return lcs(text1, text2, 0, 0);
+        int[][] dp = new int[text2.length() + 1][text1.length() + 1];
+
+        for (int y = 1; y < dp.length; y++) {
+            for (int x = 1; x < dp[0].length; x++) {
+                if (text2.charAt(y - 1) == text1.charAt(x - 1)) {
+                    dp[y][x] = dp[y - 1][x - 1] + 1;
+                } else {
+                    dp[y][x] = Math.max(dp[y - 1][x], dp[y][x - 1]);
+                }
+            }
+        }
+
+        return dp[text2.length()][text1.length()];
+
     }
+    // Runtime: 1305 ms, faster than 5.05% Memory Usage: 186.3 MB, less than 100.00%
+    // public class Memo {
+    // int index1;
+    // int index2;
 
-    public int lcs(String text1, String text2, int index1, int index2) {
-        if (textToLcs.containsKey(new Memo(index1, index2))) {
-            return textToLcs.get(new Memo(index1, index2));
-        }
-        if (index1 >= text1.length() || index2 >= text2.length()) {
-            return 0;
-        }
+    // public Memo(int index1, int index2) {
+    // this.index1 = index1;
+    // this.index2 = index2;
+    // }
 
-        int maxLcs = 0;
-        if (text1.charAt(index1) == text2.charAt(index2)) {
-            int lcs = lcs(text1, text2, index1 + 1, index2 + 1) + 1;
-            maxLcs = Math.max(maxLcs, lcs);
-        } else {
-            int lcs1 = lcs(text1, text2, index1 + 1, index2);
-            maxLcs = Math.max(maxLcs, lcs1);
-            int lcs2 = lcs(text1, text2, index1, index2 + 1);
-            maxLcs = Math.max(maxLcs, lcs2);
-        }
+    // @Override
+    // public int hashCode() {
+    // final int prime = 31;
+    // int result = 1;
+    // result = prime * result + getEnclosingInstance().hashCode();
+    // result = prime * result + index1;
+    // result = prime * result + index2;
+    // return result;
+    // }
 
-        textToLcs.put(new Memo(index1, index2), maxLcs);
+    // @Override
+    // public boolean equals(Object obj) {
+    // if (this == obj)
+    // return true;
+    // if (obj == null)
+    // return false;
+    // if (getClass() != obj.getClass())
+    // return false;
+    // Memo other = (Memo) obj;
+    // if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
+    // return false;
+    // if (index1 != other.index1)
+    // return false;
+    // if (index2 != other.index2)
+    // return false;
+    // return true;
+    // }
 
-        return maxLcs;
-    }
+    // private LongestCommonSubsequence getEnclosingInstance() {
+    // return LongestCommonSubsequence.this;
+    // }
+
+    // }
+
+    // HashMap<Memo, Integer> textToLcs = new HashMap<>();
+
+    // public int longestCommonSubsequence(String text1, String text2) {
+
+    // return lcs(text1, text2, 0, 0);
+    // }
+
+    // public int lcs(String text1, String text2, int index1, int index2) {
+    // if (textToLcs.containsKey(new Memo(index1, index2))) {
+    // return textToLcs.get(new Memo(index1, index2));
+    // }
+    // if (index1 >= text1.length() || index2 >= text2.length()) {
+    // return 0;
+    // }
+
+    // int maxLcs = 0;
+    // if (text1.charAt(index1) == text2.charAt(index2)) {
+    // int lcs = lcs(text1, text2, index1 + 1, index2 + 1) + 1;
+    // maxLcs = Math.max(maxLcs, lcs);
+    // } else {
+    // int lcs1 = lcs(text1, text2, index1 + 1, index2);
+    // maxLcs = Math.max(maxLcs, lcs1);
+    // int lcs2 = lcs(text1, text2, index1, index2 + 1);
+    // maxLcs = Math.max(maxLcs, lcs2);
+    // }
+
+    // textToLcs.put(new Memo(index1, index2), maxLcs);
+
+    // return maxLcs;
+    // }
 
     // public class Memo {
     // String text1;
