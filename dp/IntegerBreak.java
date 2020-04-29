324d71cf7b3f69bb8f8a3d48b243346b03d2c6cd
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-04-29 19:39:06 +0300

    Runtime: 0 ms, faster than 100.00% Memory Usage: 36.1 MB, less than 14.29%

--- dp/IntegerBreak.java
+++ dp/IntegerBreak.java
@@ -1,5 +1,6 @@
 package leetcode.dp;
 
+/* https://leetcode.com/problems/integer-break */
 public class IntegerBreak {
 
     int[] solution = new int[58];
@@ -19,6 +20,19 @@ public class IntegerBreak {
         }
 
         int max = n - 1;
+
+        // iterated between 2 and 3 since maximal products are derive from these
+        // multipliers
+
+        /*
+         * Now we know from maxima-minima concept that, If an integer need to break in
+         * two parts, then to maximize their product those part should be equal. Using
+         * this concept lets break n into (n/x) x’s then their product will be x(n/x),
+         * now if we take derivative of this product and make that equal to 0 for
+         * maxima, we will get to know that value of x should be e (base of the natural
+         * logarithm) for maximum product. As we know that 2 < e < 3, so we should break
+         * every Integer into 2 or 3 only for maximum product.
+         */
         for (int i = 2; i < 4; i++) {
             max = Math.max(max, i * integerBreak(n - i));
             max = Math.max(max, i * (n - i));
