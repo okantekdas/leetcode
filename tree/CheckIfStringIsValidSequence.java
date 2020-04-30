311acf050e9deaf312fdb3b87fde1d1c5355ef3d
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-04-30 11:41:52 +0300

    Runtime: 0 ms Memory Usage: 40.2 MB

---
+++ tree/CheckIfStringIsValidSequence.java
@@ -0,0 +1,162 @@
+package leetcode.tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/* https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/532/week-5/3315/ */
+public class CheckIfStringIsValidSequence {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isValidSequence(TreeNode root, int[] arr) {
+
+        return calculateIsValidSequence(root, arr, 0);
+    }
+
+    public boolean calculateIsValidSequence(TreeNode root, int[] arr, int index) {
+
+        if (index > arr.length - 1) {
+            return false;
+        }
+
+        if (root == null) {
+            return false;
+        }
+
+        if (root.val == arr[index]) {
+            if (index == arr.length - 1 && root.left == null && root.right == null) {
+                return true;
+            }
+        }
+        if (root.val == arr[index]) {
+            return calculateIsValidSequence(root.left, arr, index + 1)
+                    || calculateIsValidSequence(root.right, arr, index + 1);
+        } else if (root.val == arr[0]) {
+            return calculateIsValidSequence(root.left, arr, 1) || calculateIsValidSequence(root.right, arr, 1);
+        }
+
+        return false;
+    }
+
+    static CheckIfStringIsValidSequence cis = new CheckIfStringIsValidSequence();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            line = in.readLine();
+            int[] arr = stringToIntegerArray(line);
+
+            boolean ret = cis.isValidSequence(root, arr);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = cis.new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = cis.new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = cis.new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String treeNodeToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+
+        String output = "";
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (node == null) {
+                output += "null, ";
+                continue;
+            }
+
+            output += String.valueOf(node.val) + ", ";
+            nodeQueue.add(node.left);
+            nodeQueue.add(node.right);
+        }
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+}
