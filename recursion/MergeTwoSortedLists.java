5442ae08733fbd1987c0006091189f74a384577e
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-05-08 22:57:56 +0300

    Runtime: 0 ms, faster than 100.00% Memory Usage: 38.9 MB, less than 19.53%

--- recursion/MergeTwoSortedLists.java
+++ recursion/MergeTwoSortedLists.java
@@ -25,12 +25,30 @@ public class MergeTwoSortedLists {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    /* Runtime: 0 ms Memory Usage: 38.9 MB, less than 19.53% */
+    public ListNode mergeTwoListsAlternate(ListNode l1, ListNode l2) {
 
         if (l1 == null && l2 == null) {
             return null;
         }
 
-        ListNode subsolution = mergeTwoLists(l1 != null ? l1.next : null, l2 != null ? l2.next : null);
+        ListNode subsolution = mergeTwoListsAlternate(l1 != null ? l1.next : null, l2 != null ? l2.next : null);
 
         if (l1 == null) {
             subsolution = l2;
@@ -76,9 +94,7 @@ public class MergeTwoSortedLists {
                 }
             }
         }
-
         return subsolution;
-
     }
 
     static MergeTwoSortedLists mtsl = new MergeTwoSortedLists();
