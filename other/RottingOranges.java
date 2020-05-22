85276db14548046262a5a25af087e4d8aafd52f2
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-05-23 00:34:16 +0300

    Runtime: 2 ms, faster than 97.01% Memory Usage: 38.8 MB, less than 81.25%

--- other/RottingOranges.java
+++ other/RottingOranges.java
@@ -6,43 +6,17 @@ import java.util.Queue;
 /* https://leetcode.com/problems/rotting-oranges/ */
 public class RottingOranges {
 
-    class Rotten {
-        int x, y;
-
-        public Rotten(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + x;
-            result = prime * result + y;
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            Rotten other = (Rotten) obj;
-            if (x != other.x)
-                return false;
-            if (y != other.y)
-                return false;
-            return true;
-        }
-
-    }
-
     public int orangesRotting(int[][] grid) {
 
         int freshCount = 0;
-        Queue<Rotten> rottenQueue = new LinkedList<>();
+        Queue<int[]> rottenQueue = new LinkedList<>();
         for (int y = 0; y < grid.length; y++) {
             for (int x = 0; x < grid[0].length; x++) {
                 if (grid[y][x] == 2) {
-                    rottenQueue.add(new Rotten(x, y));
+                    int[] coordinate = new int[2];
+                    coordinate[0] = x;
+                    coordinate[1] = y;
+                    rottenQueue.add(coordinate);
                 } else if (grid[y][x] == 1) {
                     freshCount++;
                 }
@@ -58,44 +32,44 @@ public class RottingOranges {
             boolean hasRotten = false;
             for (int i = 0; i < queueSize; i++) {
 
-                Rotten rotten = rottenQueue.poll();
+                int[] coordinate = rottenQueue.poll();
 
-                if (rotten.y - 1 >= 0 && grid[rotten.y - 1][rotten.x] == 1) {
-                    Rotten r = new Rotten(rotten.x, rotten.y - 1);
-                    if (!rottenQueue.contains(r)) {
-                        grid[rotten.y - 1][rotten.x] = 2;
-                        rottenQueue.add(r);
-                        freshCount--;
-                        hasRotten = true;
-                    }
+                if (coordinate[1] - 1 >= 0 && grid[coordinate[1] - 1][coordinate[0]] == 1) {
+                    grid[coordinate[1] - 1][coordinate[0]] = 2;
+                    int[] c = new int[2];
+                    c[0] = coordinate[0];
+                    c[1] = coordinate[1] - 1;
+                    rottenQueue.add(c);
+                    freshCount--;
+                    hasRotten = true;
 
                 }
-                if (rotten.y + 1 < grid.length && grid[rotten.y + 1][rotten.x] == 1) {
-                    Rotten r = new Rotten(rotten.x, rotten.y + 1);
-                    if (!rottenQueue.contains(r)) {
-                        grid[rotten.y + 1][rotten.x] = 2;
-                        rottenQueue.add(r);
-                        freshCount--;
-                        hasRotten = true;
-                    }
+                if (coordinate[1] + 1 < grid.length && grid[coordinate[1] + 1][coordinate[0]] == 1) {
+                    grid[coordinate[1] + 1][coordinate[0]] = 2;
+                    int[] c = new int[2];
+                    c[0] = coordinate[0];
+                    c[1] = coordinate[1] + 1;
+                    rottenQueue.add(c);
+                    freshCount--;
+                    hasRotten = true;
                 }
-                if (rotten.x - 1 >= 0 && grid[rotten.y][rotten.x - 1] == 1) {
-                    Rotten r = new Rotten(rotten.x - 1, rotten.y);
-                    if (!rottenQueue.contains(r)) {
-                        grid[rotten.y][rotten.x - 1] = 2;
-                        rottenQueue.add(r);
-                        freshCount--;
-                        hasRotten = true;
-                    }
+                if (coordinate[0] - 1 >= 0 && grid[coordinate[1]][coordinate[0] - 1] == 1) {
+                    grid[coordinate[1]][coordinate[0] - 1] = 2;
+                    int[] c = new int[2];
+                    c[0] = coordinate[0] - 1;
+                    c[1] = coordinate[1];
+                    rottenQueue.add(c);
+                    freshCount--;
+                    hasRotten = true;
                 }
-                if (rotten.x + 1 < grid[0].length && grid[rotten.y][rotten.x + 1] == 1) {
-                    Rotten r = new Rotten(rotten.x + 1, rotten.y);
-                    if (!rottenQueue.contains(r)) {
-                        grid[rotten.y][rotten.x + 1] = 2;
-                        rottenQueue.add(r);
-                        freshCount--;
-                        hasRotten = true;
-                    }
+                if (coordinate[0] + 1 < grid[0].length && grid[coordinate[1]][coordinate[0] + 1] == 1) {
+                    grid[coordinate[1]][coordinate[0] + 1] = 2;
+                    int[] c = new int[2];
+                    c[0] = coordinate[0] + 1;
+                    c[1] = coordinate[1];
+                    rottenQueue.add(c);
+                    freshCount--;
+                    hasRotten = true;
                 }
 
             }
@@ -110,9 +84,9 @@ public class RottingOranges {
 
     public static void main(String[] args) {
 
-        // int[][] grid = { { 2, 1, 1 }, { 1, 1, 0 }, { 0, 1, 1 } };
+        int[][] grid = { { 2, 1, 1 }, { 1, 1, 0 }, { 0, 1, 1 } };
         // int[][] grid = { { 2, 1, 1 }, { 0, 1, 1 }, { 1, 0, 1 } };
-        int[][] grid = { { 0, 1 } };
+        // int[][] grid = { { 0, 1 } };
 
         System.out.println(new RottingOranges().orangesRotting(grid));
     }
