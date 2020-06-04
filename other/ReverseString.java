d6e775bf7e887332a40aab2203680c2b302b82c5
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-06-04 10:37:44 +0300

    Runtime: 0 ms, faster than 100.00% Memory Usage: 46.1 MB, less than 96.45%

---
+++ other/ReverseString.java
@@ -0,0 +1,31 @@
+package leetcode.other;
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+
+        if (s == null || s.length == 0) {
+            return;
+        }
+
+        int right = s.length - 1;
+        int left = 0;
+        while(left<right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        char[] s = { 'h', 'e', 'l', 'l', 'o' };
+
+        new ReverseString().reverseString(s);
+
+        System.out.println(s);
+    }
+
+}
