9bc1bf0ff2a762849c2725aa17e224a3d7515d50
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-06-09 19:06:31 +0300

    Runtime: 1 ms, faster than 92.89%

---
+++ dp/IsSubsequence.java
@@ -0,0 +1,29 @@
+package leetcode.dp;
+
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        char firstChar = s.charAt(0);
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) == firstChar) {
+                return isSubsequence(s.substring(1), t.substring(i + 1));
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "axc";
+        String t = "ahbgdc";
+        System.out.println(new IsSubsequence().isSubsequence(s, t));
+    }
+
+}
