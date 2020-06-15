18ba192accb8a0b56c05ff6d25e0c911281784d9
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-06-15 12:45:15 +0300

    Runtime: 0 ms

---
+++ tree/SearchInABinarySearchTree.java
@@ -0,0 +1,41 @@
+package leetcode.tree;
+
+public class SearchInABinarySearchTree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+
+    }
+
+}
