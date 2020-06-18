a32b192fb77036605522f367894f610e3abcacfc
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-06-18 20:48:21 +0300

    https://leetcode.com/problems/h-index Memory Usage: 37.2 MB, less than 80.28%

---
+++ search/HIndex.java
@@ -0,0 +1,45 @@
+package leetcode.search;
+
+public class HIndex {
+    public int hIndex(int[] citations) {
+        if (citations == null) {
+            return 0;
+        }
+
+        Arrays.sort(citations);
+        return calcualteIndex(citations, 0, citations.length - 1);
+    }
+
+    public int calcualteIndex(int[] citations, int left, int right) {
+        int middle = left + (right - left) / 2;
+
+        if (left > right) {
+            return 0;
+        }
+        if (left == right) {
+            if (citations[left] == 0) {
+                return 0;
+            }
+
+            return Math.min(citations[left], citations.length - left);
+        }
+
+        int max = 0;
+
+        if (citations[middle] == citations.length - middle) {
+            return citations.length - middle;
+        }
+
+        if (citations[middle] < citations.length - middle) {
+            return calcualteIndex(citations, middle + 1, right);
+        }
+        if (citations[middle] > citations.length - middle) {
+            max = citations.length - middle;
+            int solution = calcualteIndex(citations, left, middle - 1);
+            max = Math.max(solution, max);
+        }
+
+        return max;
+
+    }
+}
