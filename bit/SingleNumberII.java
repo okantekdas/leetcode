9d7c3f5618afa5c47158e841a42bb86052f35fb6
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-06-22 23:10:24 +0300

    Runtime: 4 ms, faster than 37.60% Memory Usage: 39.1 MB, less than 75.71%

---
+++ bit/SingleNumberII.java
@@ -0,0 +1,25 @@
+package leetcode.bit;
+
+/* https://leetcode.com/problems/single-number-ii/ */
+public class SingleNumberII {
+
+    public int singleNumber(int[] nums) {
+
+        int solution = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (((nums[j] >> i) & 1) == 1) {
+                    sum++;
+                    sum %= 3;
+                }
+            }
+            if (sum != 0) {
+                solution |= sum << i;
+            }
+        }
+
+        return solution;
+    }
+
+}
