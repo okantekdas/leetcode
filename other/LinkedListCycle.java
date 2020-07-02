73a8312c251c493aa58adfee9949212aae5e6c6b
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-07-02 12:10:54 +0300

    Runtime: 0 ms, faster than 100.00% Memory Usage: 38.9 MB, less than 99.96%

---
+++ other/LinkedListCycle.java
@@ -0,0 +1,41 @@
+package leetcode.other;
+
+/* https://leetcode.com/problems/linked-list-cycle/ */
+public class LinkedListCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null) {
+            return false;
+        }
+
+        ListNode left = head;
+        ListNode right = head.next;
+
+        while (right != null) {
+            if (left == right) {
+                return true;
+            }
+            left = left.next;
+            if (right.next == null || right.next.next == null) {
+                return false;
+            }
+            right = right.next.next;
+
+        }
+
+        return false;
+
+    }
+
+}
