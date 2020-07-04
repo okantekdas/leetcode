cd89fa81f541e9391129aeeec1fab9251e0fef3b
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-07-05 01:00:22 +0300

    Runtime: 2 ms, faster than 70.00%

---
+++ other/TwoSum.java
@@ -0,0 +1,22 @@
+package leetcode.other;
+
+import java.util.HashMap;
+
+/* https://leetcode.com/problems/two-sum/ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numToIndexes = new HashMap<>();
+
+        int[] solution = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (numToIndexes.containsKey(target - nums[i])) {
+                solution[0] = numToIndexes.get(target - nums[i]);
+                solution[1] = i;
+                return solution;
+            }
+            numToIndexes.put(nums[i], i);
+        }
+        return solution;
+    }
+
+}
