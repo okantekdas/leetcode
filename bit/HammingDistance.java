b13bf76968126cc75d3df351dc9371948ddb7373
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-07-05 14:13:25 +0300

    Runtime: 0 ms, faster than 100.00%

---
+++ bit/HammingDistance.java
@@ -0,0 +1,16 @@
+package leetcode.bit;
+
+public class HammingDistance {
+    public int hammingDistance(int x, int y) {
+
+        int counter = 0;
+        for (int i = 0; i < 32; i++) {
+            if (((x >> i) & 1) != ((y >> i) & 1)) {
+                counter++;
+            }
+
+        }
+
+        return counter;
+    }
+}
