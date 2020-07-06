f20249ef40a0dbf4b5786abcb18524b09f40e0b7
Okan Tekdas <okan.tekdas@argela.com.tr>
2020-07-06 17:29:57 +0300

    Runtime: 0 ms, faster than 100.00%

---
+++ other/PlusOne.java
@@ -0,0 +1,29 @@
+package leetcode.other;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class PlusOne {
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+
+        return digits;
+
+    }
+
+    public static void main(String[] args) {
+        int[] digits = { 5, 2, 2, 6, 5, 7, 1, 9, 0, 3, 8, 6, 8, 6, 5, 2, 1, 8, 7, 9, 8, 3, 8, 4, 7, 2, 5, 8, 9 };
+        System.out.println(Arrays.toString(new PlusOne().plusOne(digits)));
+    }
+
+}
