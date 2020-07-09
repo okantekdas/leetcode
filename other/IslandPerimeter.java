26eec6698b532aa9ca3e6db5ad9ea5e6c2332117
okante <okan.tekdas@argela.com.tr>
2020-07-09 20:06:33 +0300

    Runtime: 10 ms, faster than 46.65% Memory Usage: 39.8 MB

---
+++ other/IslandPerimeter.java
@@ -0,0 +1,38 @@
+
+/* https://leetcode.com/problems/island-perimeter/ */
+public class IslandPerimeter {
+
+    public int islandPerimeter(int[][] grid) {
+
+        int perimeter = 0;
+
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[0].length; x++) {
+                if (grid[y][x] == 1) {
+                    perimeter += 4;
+                    if (y > 0) {
+                        if (grid[y - 1][x] == 1) {
+                            perimeter--;
+                        }
+                    }
+                    if (y < grid.length - 1) {
+                        if (grid[y + 1][x] == 1) {
+                            perimeter--;
+                        }
+                    }
+                    if (x > 0) {
+                        if (grid[y][x - 1] == 1) {
+                            perimeter--;
+                        }
+                    }
+                    if (x < grid[0].length - 1) {
+                        if (grid[y][x + 1] == 1) {
+                            perimeter--;
+                        }
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+}
