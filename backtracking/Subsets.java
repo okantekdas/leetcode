012bf4b478c81b1546be733a6dea17958235e07e
okante <okan.tekdas@argela.com.tr>
2020-07-11 15:15:42 +0300

    Runtime: 1 ms, faster than 67.41%

---
+++ backtracking/Subsets.java
@@ -0,0 +1,53 @@
+package leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* https://leetcode.com/problems/subsets/ */
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+
+        return subsets(nums, 0);
+
+    }
+
+    public List<List<Integer>> subsets(int[] nums, int index) {
+
+        List<List<Integer>> subsets = new ArrayList<>();
+
+        if (index == nums.length) {
+            List<Integer> subset = new ArrayList<>();
+            subsets.add(subset);
+            return subsets;
+        }
+
+        List<List<Integer>> subSubsets = subsets(nums, index + 1);
+        List<List<Integer>> cartesianProduct = cartesianProduct(nums[index], subSubsets);
+        subsets.addAll(cartesianProduct);
+        subsets.addAll(subSubsets);
+
+        return subsets;
+
+    }
+
+    private List<List<Integer>> cartesianProduct(int number, List<List<Integer>> subSubsets) {
+
+        List<List<Integer>> cartesianProduct = new ArrayList<>();
+
+        for (List<Integer> subset : subSubsets) {
+            List<Integer> cartesianSubset = new ArrayList<>(subset);
+            cartesianSubset.add(number);
+            cartesianProduct.add(cartesianSubset);
+        }
+        return cartesianProduct;
+    }
+
+}
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3 };
+
+        List<List<Integer>> subsets = new Subsets().subsets(nums);
+        System.out.println();
+    }
+}
