1eb357b8eb2e449e3f66a19f26ca502a8ce6506c
okante <okan.tekdas@argela.com.tr>
2020-09-23 19:41:45 +0300

    Runtime: 0 ms, faster than 100.00% Memory Usage: 39.6 MB, less than 85.21%

---
+++ greedy/GasStation.java
@@ -0,0 +1,51 @@
+package leetcode.greedy;
+
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        
+        int totalGas = 0;
+        int totalCost = 0;
+        int netCost = 0;
+        int[] gasToBeCollected = new int[cost.length];
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<gas.length; i++){
+            totalGas += gas[i];
+            totalCost += cost[i];
+            netCost += gas[i]-cost[i];
+            
+            min = Math.min(min, netCost);
+            
+        }
+        
+        for(int i = gas.length-1; i>+0; i--){
+            if(i== gas.length-1){
+                
+                gasToBeCollected[i] = gas[i] - cost[i];
+            }
+            else{
+                gasToBeCollected[i] = gasToBeCollected[i+1] + gas[i] - cost[i] ;
+            }
+        }
+        
+        if(totalCost>totalGas){
+            return -1;
+        }
+        
+        
+        for(int i=cost.length-1; i>=0; i--){
+            if(-min<=gasToBeCollected[i]){
+                return i;
+            }
+        }
+        
+        return -1;
+        
+    }
+
+    public static void main(String[] args) {
+        int[]gas = {1,2,3,4,5};
+        int[]cost = {3,4,5,1,2};
+
+        System.out.println(new GasStation().canCompleteCircuit(gas, cost));
+    }
+}
