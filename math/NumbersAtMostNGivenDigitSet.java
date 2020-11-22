dbed3b51da3aa1fff3e2852cc44cd8fe39687013
Okan Tekdaş <okan.tekdas@mildmania.com>
2020-11-22 22:11:56 +0300

    Runtime: 1 ms, faster than 58.12% Memory Usage: 35.8 MB, less than 99.15%

---
+++ math/NumbersAtMostNGivenDigitSet.java
@@ -0,0 +1,68 @@
+package leetcode.math;
+
+import java.util.Arrays;
+
+/* https://leetcode.com/problems/numbers-at-most-n-given-digit-set/ */
+/* Runtime: 1 ms, faster than 58.12% Memory Usage: 36.1 MB, less than 85.47% */
+public class NumbersAtMostNGivenDigitSet {
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+
+        int[] digitsNum = new int[digits.length];
+
+        int i = 0;
+        for (String digit : digits) {
+            digitsNum[i++] = Integer.valueOf(digit);
+        }
+        Arrays.sort(digitsNum);
+        int count = 0;
+        int length = String.valueOf(n).length();
+        for (i = 1; i < length; i++) {
+            count += Math.pow(digits.length, i);
+        }
+
+        int sum = -1;
+
+        for (i = 0; i < length; i++) {
+            int currentDigit = n % 10;
+            int smallerCount = 0;
+            int j = 0;
+            boolean isSame = false;
+            for (; j < digitsNum.length; j++) {
+
+                if (currentDigit == digitsNum[j]) {
+                    smallerCount++;
+                    isSame = true;
+                    break;
+                } else if (currentDigit < digitsNum[j]) {
+                    break;
+                } else {
+                    smallerCount++;
+                }
+            }
+            n -= currentDigit;
+            n /= 10;
+
+            if (sum == -1) {
+                sum = smallerCount;
+            } else {
+                if (smallerCount == 0) {
+                    sum = 0;
+                } else {
+                    if (isSame) {
+                        sum = (int) Math.pow(digitsNum.length, i) * (smallerCount - 1) + sum;
+                    } else {
+                        sum = (int) Math.pow(digitsNum.length, i) * smallerCount;
+                    }
+                }
+            }
+        }
+        return count + sum;
+    }
+
+    public static void main(String[] args) {
+        String[] digits = { "2", "3", "4", "6", "8" };
+        int n = 61;
+
+        System.out.println(new NumbersAtMostNGivenDigitSet().atMostNGivenDigitSet(digits, n));
+    }
+}
