cd787697ac4cc99ff2899413cc9da08108e09a1d
Okan Tekdaş <okan.tekdas@mildmania.com>
2020-12-06 13:40:32 +0300

    Runtime: 1 ms, faster than 50.73% Memory Usage: 38.5 MB, less than 85.03%

---
+++ tree/PopulatingNextRightPointersInEachNode2.java
@@ -0,0 +1,61 @@
+package leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/* Runtime: 1 ms, faster than 50.73% Memory Usage: 38.5 MB, less than 85.03% */
+public class PopulatingNextRightPointersInEachNode2 {
+
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    };
+
+    public Node connect(Node root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node currentNode = queue.poll();
+
+                if (i < size - 1) {
+                    currentNode.next = queue.peek();
+                }
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
+
+                }
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
+                }
+
+            }
+        }
+        return root;
+    }
+}
