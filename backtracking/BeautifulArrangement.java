0c7a50bb65ea4041ecadad4d9252f0b07942ac1a
Okan <okantekdas@gmail.com>
2021-01-03 16:52:37 +0300

    Runtime: 62 ms, faster than 76.36% Memory Usage: 35.8 MB, less than 60.52%

--- backtracking/BeautifulArrangement.java
+++ backtracking/BeautifulArrangement.java
@@ -1,27 +1,27 @@
 package leetcode.backtracking;
 
 /* https://leetcode.com/problems/beautiful-arrangement/ */
-/* Runtime: 65 ms, faster than 69.57% Memory Usage: 35.8 MB, less than 60.52%  */
+/* Runtime: 62 ms, faster than 76.36% Memory Usage: 35.8 MB, less than 60.52% */
 public class BeautifulArrangement {
+    int count = 0;
+
     public int countArrangement(int n) {
 
         boolean[] visited = new boolean[n + 1];
-        int count = 0;
-        countArrangementHelper(n, 1, visited, count);
+        countArrangementHelper(n, 1, visited);
         return count;
     }
 
-    public void countArrangementHelper(int n, int position, boolean[] visited, int count) {
-
+    public void countArrangementHelper(int n, int position, boolean[] visited) {
         if (position > n) {
             count++;
             return;
         }
 
-        for (int i = 1; i < n; i++) {
+        for (int i = n; i > 0; i--) {
             if (!visited[i] && (i % position == 0 || position % i == 0)) {
                 visited[i] = true;
-                countArrangementHelper(n, position + 1, visited, count);
+                countArrangementHelper(n, position + 1, visited);
                 visited[i] = false;
             }
         }
